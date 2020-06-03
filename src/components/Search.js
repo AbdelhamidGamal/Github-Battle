2b79c311cca7de2ad11f06e95d8ddad12c5d6aad
abdelhameed <3b7ameed@gmail.com>
2020-06-04 00:49:11 +0200

    improving search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
-import Nav from "./Nav";
-import SelectUser from "./SelectUser";
-import UserInfo from "./UserInfo";
+import React, { useState } from 'react';
+import Nav from './Nav';
+import SelectUser from './SelectUser';
+import UserInfo from './UserInfo';
 
 function Search() {
   const [userInfo, setUserInfo] = useState({});
+  const [reposInfo, setReposInfo] = useState([]);
+
+  const handleUserInfo = async (userInf) => {
+    setUserInfo(userInf);
+    const res = await fetch(
+      `https://api.github.com/users/${userInf.login}/repos?sort=created&direction=desc`
+    );
+    const data = await res.json();
+    setReposInfo(data);
+    console.log(data);
+  };
 
   if (userInfo.login) {
     return (
       <>
         <Nav />
-        <div className="centerItemsInDiv searchPage">
+        <div className='centerItemsInDiv searchPage'>
           <UserInfo user={userInfo} />
+        </div>
+        <div className='reposGrid'>
+          {reposInfo &&
+            reposInfo.map((repo) => (
+              <div className='repo' key={repo.id}>
+                <h3>{repo.name}</h3>
+                <div>{repo.description}</div>
+                <div>
+                  Stars : {repo.stargazers_count}
+                  Forks : {repo.forks_count}
+                </div>
+                <div>Language : {repo.language}</div>
+                <button className='btn'>
+                  {' '}
+                  <a href={repo.html_url}>Repo</a>{' '}
+                </button>
+              </div>
+            ))}
         </div>
       </>
     );
@@ -20,7 +49,7 @@ function Search() {
   return (
     <>
       <Nav />
-      <SelectUser setPlayer={setUserInfo} />
+      <SelectUser setPlayer={handleUserInfo} />
     </>
   );
 }
