e46f6a0bff20f0b251f60fc0aa87ab4378b2557a
Abdelhamid Ismail <3b7ameed@gmail.com>
2020-06-03 05:09:37 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App /> , document.querySelector('#root'))
